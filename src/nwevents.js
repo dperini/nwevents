415ef056e1a837b2773d7fad188f8530ea9547f5
Diego Perini <diego.perini@gmail.com>
2011-06-05 00:10:21 +0200

    improved internal '.match()' method, added use of native 'matchesSelector' where available, if passed a declarative object instead of a selector string do a recursive object matching on element properties

--- src/nwevents.js
+++ src/nwevents.js
@@ -1164,85 +1164,111 @@
 })(this);
 
 // embedded NW.Dom.match() so basic event delegation works,
-// overwritten if loading the nwmatcher.js selector engine
+// accepts mixed id, tag, class simple selectors div#foo.bar
+// accepts objects having properties that must match element
+// overwritten when loading the nwmatcher.js selector engine
 (function(global) {
 
-  var Patterns = {
+  var version = 'match-1.0',
+
+  Dom = typeof exports == 'object' ? exports : (
+    (global.NW || (global.NW = { })) &&
+    (global.NW.Dom || (global.NW.Dom = { }))),
+
+  doc = global.document,
+  root = document.documentElement,
+
+  Patterns = {
     'id': /#([^\.]+)/,
     'tagName': /^([^#\.]+)/,
     'className': /\.([^#]+)/,
     'all': /^[\.\-\#\w]+$/
-  };
+  },
 
-  global.NW || (global.NW = { });
+  References = {
+    'parentNode': 1,
+    'lastChild': 1,
+    'firstChild': 1,
+    'nextSibling': 1,
+    'previousSibling': 1,
+    'lastElementChild': 1,
+    'firstElementChild': 1,
+    'nextElementSibling': 1,
+    'previousElementSibling': 1
+  },
 
-  NW.Dom = ({
-    // use a simple selector match or a full
-    // CSS3 selector engine if it is available
-    match:
-      function(element, selector) {
-        var d, j, id, length, results, tagName, className, match, matched = false;
-        d = element.ownerDocument || element;
-        if (typeof selector == 'string') {
-          if (typeof d.querySelectorAll != 'undefined') {
-            try {
-              results = d.querySelectorAll(selector);
-            } catch(e) {
-              results = [];
+  // select Matches Selector API to use if available
+  NATIVE_MATCHES_SELECTOR =
+    'matchesSelector' in root ? 'matchesSelector' :
+    'oMatchesSelector' in root ? 'oMatchesSelector' :
+    'msMatchesSelector' in root ? 'msMatchesSelector' :
+    'mozMatchesSelector' in root ? 'mozMatchesSelector' :
+    'webkitMatchesSelector' in root ? 'webkitMatchesSelector' : null,
+
+  RE_SIMPLE_SELECTOR = RegExp('^(?:\\*|[.#]?-?[_a-zA-Z]{1}(?:[-\\w]|[^\\x00-\\xa0]|\\\\.)*)$');
+
+  // use a simple selector match or a full
+  // CSS3 selector engine if it is available
+  Dom.match = function(element, selector) {
+
+    var d, j, length,
+    id, tagName, className,
+    match, matched = false, results = [ ];
+
+    d = element.ownerDocument || element;
+
+    if (typeof selector == 'string') {
+      if (RE_SIMPLE_SELECTOR.test(selector)) {
+        // use a simple selector match (id, tag, class)
+        if (selector.match(Patterns.all)) {
+          match = selector.match(Patterns.tagName);
+          tagName = match ? match[1] : '*';
+          match = selector.match(Patterns.id);
+          id = match ? match[1] : null;
+          match = selector.match(Patterns.className);
+          className = match ? match[1] : null;
+          if ((!id || id == element.id) &&
+            (!tagName || tagName == '*' || (new RegExp(tagName, 'i')).test(element.nodeName)) &&
+            (!className || (' ' + element.className.replace(/\s+/g, ' ') + ' ').indexOf(' ' + className + ' ') > -1)) {
+            matched = true;
+          }
+        }
+      } else if (NATIVE_MATCHES_SELECTOR) {
+        // use native matchesSelector where available
+        return element[NATIVE_MATCHES_SELECTOR](selector);
+      }
+      return false;
+    } else {
+      // a selector matcher object
+      if (typeof selector == 'object') {
+        // match on property/values
+        for (j in selector) {
+          matched = false;
+          if (j == 'className') {
+            // handle special className matching
+            if ((' ' + element.className.replace(/\s+/g, ' ') + ' ').indexOf(' ' + selector[j] + ' ') > -1) {
+              matched = true;
             }
-            length = results.length;
-            while (length) {
-              length--;
-              if (results[length] === element) {
-                matched = true;
-                break;
-              }
+          } else if (j == 'nodeName' || j == 'tagName') {
+            // handle upper/lower case tagName
+            if (element.nodeName.toLowerCase() == selector[j].toLowerCase()) {
+              matched = true;
             }
-          } else if (selector.match(Patterns.all)) {
-            // use a simple selector match (id, tag, class)
-            match = selector.match(Patterns.tagName);
-            tagName = match ? match[1] : '*';
-            match = selector.match(Patterns.id);
-            id = match ? match[1] : null;
-            match = selector.match(Patterns.className);
-            className = match ? match[1] : null;
-            if ((!id || id == element.id) &&
-              (!tagName || tagName == '*' || (new RegExp(tagName, 'i')).test(element.nodeName)) &&
-              (!className || (' ' + element.className + ' ').replace(/\s\s+/g, ' ').indexOf(' ' + className + ' ') > -1)) {
+          } else if (References[j]) {
+            // handle matching other references
+            matched = Dom.match(element[j], selector[j]);
+          } else {
+            // handle matching other properties
+            if (element[j] === selector[j]) {
               matched = true;
             }
           }
-        } else {
-          // a selector matcher object
-          if (typeof selector == 'object') {
-            // match on property/values
-            for (j in selector) {
-              if (j == 'className') {
-                // handle special className matching
-                if ((' ' + element.className + ' ').replace(/\s\s+/g,' ').indexOf(' ' + selector[j] + ' ') > -1) {
-                  matched = true;
-                  break;
-                }
-              } else if (j == 'nodeName' || j == 'tagName') {
-                // handle upper/lower case tagName
-                if (element.nodeName.toLowerCase() == selector[j].toLowerCase()) {
-                  matched = true;
-                  break;
-                }
-              } else {
-                // handle matching other properties
-                if (element[j] === selector[j]) {
-                  matched = true;
-                  break;
-                }
-              }
-            }
-          }
+          results.push(matched);
         }
-        // boolean true/false
-        return matched;
       }
-
-  });
+    }
+    // boolean true/false
+    return results.join('|').indexOf('false') < 0;
+  };
 
 })(this);
